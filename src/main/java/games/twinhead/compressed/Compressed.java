d256a2c82e711c8c54c62811066792d9e51bd7e5
Nibaru <nickschell.42@gmail.com>
2023-06-14 18:14:13 -0400

    l4j import removal

--- src/main/java/games/twinhead/compressed/Compressed.java
+++ src/main/java/games/twinhead/compressed/Compressed.java
@@ -10,8 +10,6 @@ import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Compressed implements ModInitializer {
 
